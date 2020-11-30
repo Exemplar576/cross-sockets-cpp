1edf16e70983b7cf3260122066ddf5df4650ae6c
Exemplar576 <75015344+Exemplar576@users.noreply.github.com>
2020-11-30 15:19:10 +0000

    Update EchoClient.cpp

--- Examples/EchoClient.cpp
+++ Examples/EchoClient.cpp
@@ -2,25 +2,25 @@
 #include <iostream>
 #include <string>
 
-int maain()
+int main()
 {
-	Socket s(Internetwork, Stream, TCP);
-	while (true)
-	{
-		try
-		{
-			s.Connect("10.86.7.182", 3000);
-			while (true)
-			{
-				std::string msg;
-				std::cin >> msg;
-				s.Send(msg);
-				std::cout << s.Receive(4096) << std::endl;
-			}
-		}
-		catch (SocketException e)
-		{
-			std::cerr << e.what() << std::endl;
-		}
+    Socket s(Internetwork, Stream, TCP);
+    while (true)
+    {
+        try
+        {
+	    s.Connect("10.86.7.182", 3000);
+	    while (true)
+	    {
+	        std::string msg;
+	        std::cin >> msg;
+		s.Send(msg);
+		std::cout << s.Receive(4096) << std::endl;
+	    }
 	}
+	catch (SocketException e)
+	{
+	    std::cerr << e.what() << std::endl;
+	}
+    }
 }
