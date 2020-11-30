5773683e03d0aa5705dfd4630fb547ee5bc02c6d
Exemplar576 <75015344+Exemplar576@users.noreply.github.com>
2020-11-30 15:00:27 +0000

    Add files via upload

---
+++ Socket.cpp
@@ -0,0 +1,263 @@
+#include "Socket.hpp"
+#include <memory>
+#include <fstream>
+
+int Socket::listen_s;
+fd_set Socket::master;
+std::vector<int> Socket::fd;
+
+void Socket::Init(const char* HOST, int PORT)
+{
+#ifdef WIN32
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		throw SocketException("[SocketError] Failed to create socket, perhaps the library is not supported on this OS.");
+	}
+#endif
+	s = socket(AF, TYPE, PROTOCOL);
+	sockaddr_in addr;
+	inet_pton(AF, HOST, &addr.sin_addr.s_addr);
+	addr.sin_family = AF;
+	addr.sin_port = htons(PORT);
+	address_info = addr;
+}
+Socket::Socket() { }
+Socket::Socket(AddressFamily af, SocketType type, Protocol protocol)
+{
+	AF = af;
+	TYPE = type;
+	PROTOCOL = protocol;
+}
+void Socket::Select(std::vector<Socket>* sock_list, SelectMode mode, int time)
+{
+	fd_set copy = master;
+	std::vector<Socket> list;
+	timeval times;
+	times.tv_sec = time;
+	times.tv_usec = 0;
+	int sockCount = 0;
+	switch (mode)
+	{
+	case Read:
+		sockCount = select(0, &copy, nullptr, nullptr, !time ? nullptr : &times);
+		break;
+	case Write:
+		sockCount = select(0, nullptr, &copy, nullptr, !time ? nullptr : &times);
+		break;
+	case SelectMode::Error:
+		sockCount = select(0, nullptr, nullptr, &copy, !time ? nullptr : &times);
+		break;
+	case SelectMode::Accept:
+		sockCount = select(0, &copy, nullptr, nullptr, !time ? nullptr : &times);
+		break;
+	default:
+		throw SocketException("[SocketError] Invalid Select Flag");
+	}
+	for (int i = 0; i < fd.size(); i++)
+	{
+		if (FD_ISSET(fd[i], &copy) && ((mode == 0 || mode == 1 || mode == 2 && fd[i] != listen_s) || (mode == 3 && fd[i] == listen_s)))
+		{
+			for (int e = 0; e < sock_list->size(); e++)
+			{
+				if (sock_list->operator[](e).s == fd[i])
+				{
+					list.push_back(sock_list->operator[](e));
+				}
+			}
+		}
+	}
+	sock_list->clear();
+	for (int e = 0; e < list.size(); e++)
+	{
+		sock_list->push_back(list[e]);
+	}
+}
+bool Socket::Poll(SelectMode mode, int time)
+{
+	std::vector<Socket> sock = { *this };
+	Socket::Select(&sock, mode, time);
+	if (sock.size() > 0)
+	{
+		return true;
+	}
+	return false;
+}
+int Socket::SocketID()
+{
+	return this->s;
+}
+std::string Socket::RemoteEndpoint()
+{
+	return this->remoteEndpoint;
+}
+void Socket::Bind(std::string HOST, int PORT)
+{
+	Init(HOST.c_str(), PORT);
+	if (bind(s, (sockaddr*)&address_info, sizeof(address_info)) < 0)
+	{
+		this->Close();
+		throw SocketException("[SocketError] Failed to bind to the specified ip address and port.");
+	}
+}
+void Socket::Listen(int backlog)
+{
+	if (listen(s, backlog) < 0)
+	{  
+		this->Close();
+		throw SocketException("[SocketError] Failed to start listening.");
+	}
+	FD_ZERO(&master);
+	FD_SET(s, &master);
+	fd.push_back(s);
+	listen_s = s;
+}
+Socket Socket::Accept()
+{
+	Socket client_sock;
+	sockaddr_in client_info = { 0 };
+	socklen_t addrsize = sizeof(client_info);
+	client_sock.s = accept(s, (sockaddr*)&client_info, &addrsize);
+	if (client_sock.s <= 0)
+	{
+		this->Close();
+		throw SocketException("[SocketError] Accept Error.");
+	}
+	inet_ntop(AF, &client_info.sin_addr, client_sock.remoteEndpoint, INET_ADDRSTRLEN);
+	client_sock.client = true;
+	FD_SET(client_sock.s, &master);
+	fd.push_back(client_sock.s);
+	return client_sock;
+}
+void Socket::Connect(std::string HOST, int PORT)
+{
+	Init(HOST.c_str(), PORT);
+	if (connect(s, (sockaddr*)&address_info, sizeof(address_info)) < 0)
+	{
+		this->Close();
+		throw SocketException("[SocketError] Failed to connect to the specified ip address and port.");
+	}
+	FD_ZERO(&master);
+	FD_SET(s, &master);
+	fd.push_back(s);
+}
+void Socket::Send(std::string message)
+{
+	if (send(s, message.c_str(), message.length(), 0) < 0)
+	{
+		this->Close();
+		throw SocketException("[SocketError] Failed to send bytes to host.");
+	}
+}
+std::string Socket::Receive(int bytes)
+{
+	std::unique_ptr<char> receive_buffer(new char[bytes]);
+	int Result = recv(s, receive_buffer.get(), bytes, 0);
+	if (Result > 0)
+	{
+		std::string trim(receive_buffer.get(), (size_t)Result);
+		return trim;
+	}
+	else if (Result == 0)
+	{
+		this->Close();
+		throw SocketException("[SocketError] Connection to the host has been closed.");
+	}
+	else
+	{
+		this->Close();
+		throw SocketException("[SocketError] Failed to receive bytes from host.");
+	}
+}
+void Socket::SendTo(std::string message)
+{
+	if (sendto(s, message.c_str(), message.length(), 0, (sockaddr*)&address_info, sizeof(address_info)) < 0)
+	{
+		this->Close();
+		throw SocketException("[SocketError] Failed to send DGRAM to host.");
+	}
+}
+std::string Socket::ReceiveFrom(int bytes)
+{
+	std::unique_ptr<char> receive_buffer(new char[bytes]);
+	socklen_t addr_size = sizeof(&address_info);
+	int Result = recvfrom(s, receive_buffer.get(), bytes, 0, (sockaddr*)&address_info, &addr_size);
+	if (Result < 0)
+	{
+		this->Close();
+		throw SocketException("[SocketError] Failed to receive DGRAM packets.");
+	}
+	return std::string(receive_buffer.get(), Result);
+}
+Socket& Socket::operator<<(std::string& msg)
+{
+	this->Send(msg);
+	return *this;
+}
+Socket& Socket::operator>>(std::string& msg)
+{
+	std::string ss = this->Receive(1024);
+	while (this->Poll(Read, 1))
+	{
+		ss += this->Receive(1024);
+	}
+	msg = ss;
+	return *this;
+}
+void Socket::SendFile(std::string path)
+{
+	std::ifstream file(path, std::ios::binary);
+	file.seekg(0, std::ios::end);
+	std::streampos count = file.tellg();
+	file.seekg(0, std::ios::beg);
+	std::unique_ptr<char> ptr(new char[count]);
+	char* buffer = ptr.get();
+	file.read(buffer, count);
+	file.close();
+	while (count > 0)
+	{
+		int result = send(s, buffer, count, 0);
+		if (result < 0)
+		{
+			this->Close();
+			throw SocketException("[SocketError] Failed to send file to host.");
+		}
+		else 
+		{
+			count -= result;
+			buffer += result;
+		}
+	}
+}
+void Socket::Shutdown(How flags)
+{
+	shutdown(s, flags);
+}
+void Socket::Close()
+{
+	FD_CLR(s, &master);
+	for (int i = 0; i < fd.size(); i++)
+	{
+		if (fd[i] == s)
+		{
+			fd.erase(fd.begin() + i);
+		}
+	}
+#ifdef WIN32
+	closesocket(s);
+	if (!client)
+	{
+		WSACleanup();
+	}
+#else
+	close(s);
+#endif
+}
+SocketException::SocketException(const char* error)
+{
+	errors = error;
+}
+const char* SocketException::what()
+{
+	return this->errors;
+}
---
+++ Socket.hpp
@@ -0,0 +1,108 @@
+#ifdef WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#pragma comment(lib, "Ws2_32.lib")
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+#include <string>
+#include <vector>
+
+enum AddressFamily { Internetwork = AF_INET, InternetworkV6 = AF_INET6 };
+enum SocketType { Stream = SOCK_STREAM, DGRAM = SOCK_DGRAM };
+enum Protocol { TCP = IPPROTO_TCP, UDP = IPPROTO_UDP };
+enum SelectMode { Read = 0, Write = 1, Error = 2, Accept = 3 };
+#ifdef WIN32
+enum How { Both = SD_BOTH, Send = SD_SEND, Receive = SD_RECEIVE };
+#else
+enum How { Both = SHUTDOWN_RDWR, Send = SHUTDOWN_WR, Receive = SHUTDOWN_RD };
+#endif
+
+class Socket
+{
+private:
+	void Init(const char* HOST, int PORT);
+	int s = -1;
+	sockaddr_in address_info = { 0 };
+	int AF = AF_INET;
+	int TYPE = SOCK_STREAM;
+	int PROTOCOL = IPPROTO_TCP;
+	bool client = false;
+	char remoteEndpoint[INET_ADDRSTRLEN] = "";
+	static fd_set master;
+	static int listen_s;
+	static std::vector<int> fd;
+
+public:
+	//Instantiate a default Tcp Socket.
+	Socket();
+
+	//Instantiate a Socket with custom parameters.
+	Socket(AddressFamily af, SocketType type, Protocol protocol);
+
+	//Check array of sockets for readability, writability, exceptionals and acceptability.
+	static void Select(std::vector<Socket>* read, SelectMode mode, int timesec = 0);
+
+	//Check socket for readability, writability, exceptions and acceptability.
+	bool Poll(SelectMode mode, int timesec = 0);
+
+	//Return the underlying socket id.
+	int SocketID();
+
+	//Return the endpoint ip address of a connected client.
+	std::string RemoteEndpoint();
+
+	//Bind a socket to a set address and port.
+	void Bind(std::string HOST, int PORT);
+
+	//Listen for incoming connections.
+	void Listen(int backlog = 0);
+
+	//Accept and incoming socket connection.
+	Socket Accept();
+
+	//Connect to an endpoint with a set ip address and port.
+	void Connect(std::string HOST, int PORT);
+
+	//Send bytes to endpoint.
+	void Send(std::string message);
+
+	//Receive number of bytes from endpoint.
+	std::string Receive(int bytes);
+
+	//Send DGRAM packets
+	void SendTo(std::string message);
+
+	//Receive DGRAM packets.
+	std::string ReceiveFrom(int bytes);
+
+	//Stream operators to send message.
+	Socket& operator<<(std::string& msg);
+
+	//Stream operator to receive all available data.
+	Socket& operator>>(std::string& msg);
+
+	//Send a file over the stream.
+	void SendFile(std::string path);
+
+	//Shutdown communication.
+	void Shutdown(How flags);
+
+	//Close and cleanup the socket.
+	void Close();
+};
+
+class SocketException
+{
+private:
+	const char* errors;
+public:
+	SocketException(const char* error);
+	const char* what();
+};
